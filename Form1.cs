91afbe5edc5bf883f02eb036fe7781266ee43d55
LENOVO T450 <LENOVO T450@DESKTOP-S6J3G1K>
2023-10-31 08:07:41 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+namespace practica_7_U3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double lado1, lado2, lado3;
+            lado1 = double.Parse(textBox1.Text);
+            lado2 = double.Parse(textBox2.Text);
+            lado3 = double.Parse(textBox3.Text);
+            if ((lado1 == lado2) && (lado2 == lado3))
+                textBox4.Text = "Equilatero";
+            else
+                if ((lado1 == lado2) || (lado1 == lado3) || (lado2 == lado3))
+                textBox4.Text = "Isosceles";
+            else textBox4.Text = "Escaleno";
+        }
+    }
+}
